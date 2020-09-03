afa4d876a7907cca7ef95c15cdc81b9c7d9a2ccf
Afzal Sayed <14029371+afzalsayed96@users.noreply.github.com>
2020-09-03 18:10:41 +0530

    Update styles

--- src/Table.js
+++ src/Table.js
@@ -6,23 +6,23 @@ const Table = () => {
   return (
     <div className="min-w-full shadow overflow-hidden sm:rounded-lg border-gray-200">
       <div className="grid grid-cols-1 overflow-auto bg-white relative">
-        <table className="table-auto min-w-full divide-y divide-gray-200 col-start-1 col-span-1 row-span-1 row-start-1">
+        <table className="table-fixed min-w-full divide-y divide-gray-200 col-start-1 col-span-1 row-span-1 row-start-1">
           <thead>
             <tr>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible">
                 {Object.keys(props.consolidated_holdings[0])[0]}
               </th>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider md:invisible">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider md:invisible">
                 {Object.keys(props.consolidated_holdings[0])[1]}
               </th>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider lg:invisible">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider lg:invisible">
                 {Object.keys(props.consolidated_holdings[0])[2]}
               </th>
               {Object.keys(props.consolidated_holdings[0])
                 .slice(3, -1)
                 .map((column, index) => (
                   <th
-                    className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
                     key={index}
                   >
                     {column}
@@ -30,15 +30,15 @@ const Table = () => {
                 ))}
             </tr>
           </thead>
-          <tbody>
+          <tbody className="divide-y divide-gray-200">
             {props.consolidated_holdings.map((row, index) => (
               <tr key={index}>
-                <td className="whitespace-no-wrap px-4 py-2 invisible">
+                <td className="whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 invisible">
                   {row[Object.keys(props.consolidated_holdings[0])[0]]}
                 </td>
                 <td
                   className={
-                    "whitespace-no-wrap px-4 py-2 md:invisible" +
+                    "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 md:invisible" +
                     (typeof row[
                       Object.keys(props.consolidated_holdings[0])[1]
                     ] === "number"
@@ -52,7 +52,7 @@ const Table = () => {
                 </td>
                 <td
                   className={
-                    "whitespace-no-wrap px-4 py-2 lg:invisible" +
+                    "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 lg:invisible" +
                     (typeof row[
                       Object.keys(props.consolidated_holdings[0])[1]
                     ] === "number"
@@ -69,7 +69,7 @@ const Table = () => {
                   .map((column, cellIndex) => (
                     <td
                       className={
-                        "whitespace-no-wrap px-4 py-2" +
+                        "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3" +
                         (typeof row[column] === "number"
                           ? row[column] < 0
                             ? " text-red-500 text-right"
@@ -86,23 +86,23 @@ const Table = () => {
           </tbody>
         </table>
 
-        <table className="table-layout min-w-full divide-y divide-gray-200 sticky left-0 col-start-1 col-span-1 row-span-1 row-start-1">
+        <table className="table-fixed min-w-full divide-y divide-gray-200 sticky left-0 top-0 col-start-1 col-span-1 row-span-1 row-start-1">
           <thead>
             <tr>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider bg-white">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider bg-white border-r-2 border-gray-200 md:border-0">
                 {Object.keys(props.consolidated_holdings[0])[0]}
               </th>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible md:visible md:bg-white">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible md:visible md:bg-white border-r-2 border-gray-200 lg:border-0">
                 {Object.keys(props.consolidated_holdings[0])[1]}
               </th>
-              <th className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible lg:visible lg:bg-white">
+              <th className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible lg:visible lg:bg-white border-r-2 border-gray-200">
                 {Object.keys(props.consolidated_holdings[0])[2]}
               </th>
               {Object.keys(props.consolidated_holdings[0])
                 .slice(3, -1)
                 .map((column, index) => (
                   <th
-                    className="px-6 py-3 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible"
+                    className="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider invisible"
                     key={index}
                   >
                     {column}
@@ -110,12 +110,12 @@ const Table = () => {
                 ))}
             </tr>
           </thead>
-          <tbody>
+          <tbody className="divide-y divide-gray-200">
             {props.consolidated_holdings.map((row, index) => (
               <tr key={index}>
                 <td
                   className={
-                    "whitespace-no-wrap px-4 py-2 bg-white" +
+                    "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 bg-white border-r-2 border-gray-200 md:border-0" +
                     (typeof row[
                       Object.keys(props.consolidated_holdings[0])[0]
                     ] === "number"
@@ -129,7 +129,7 @@ const Table = () => {
                 </td>
                 <td
                   className={
-                    "whitespace-no-wrap px-4 py-2 invisible md:visible md:bg-white" +
+                    "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 invisible md:visible md:bg-white border-r-2 border-gray-200 lg:border-0" +
                     (typeof row[
                       Object.keys(props.consolidated_holdings[0])[1]
                     ] === "number"
@@ -143,7 +143,7 @@ const Table = () => {
                 </td>
                 <td
                   className={
-                    "whitespace-no-wrap px-4 py-2 invisible lg:visible lg:bg-white" +
+                    "whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 invisible lg:visible lg:bg-white border-r-2 border-gray-200" +
                     (typeof row[
                       Object.keys(props.consolidated_holdings[0])[2]
                     ] === "number"
@@ -159,7 +159,7 @@ const Table = () => {
                   .slice(3, -1)
                   .map((column, cellIndex) => (
                     <td
-                      className="whitespace-no-wrap px-4 py-2 invisible"
+                      className="whitespace-no-wrap text-sm leading-5 font-medium text-gray-900 px-6 py-3 invisible"
                       key={cellIndex}
                     >
                       {row[column]}
